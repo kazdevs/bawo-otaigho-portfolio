41afe0e09a624b30099e46d4e40090c95dd7bea4
kazdevs <kazdevs@gmail.com>
2025-02-09 18:31:34 +0100

    changes to about, contact

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { useState } from 'react'
-import './about.css'
-import AboutImg from '../../assets/bawo.jpg'
-import Skills from '../../components/skills/Skills'
-import Cv from '../../assets/BAWO_OTAIGO_RESUME.pdf'
-import data from './about-data'
+import React from "react";
+import { useState } from "react";
+import "./about.css";
+import AboutImg from "../../assets/bawo.jpg";
+import Skills from "../../components/skills/Skills";
+import Cv from "../../assets/BAWO_OTAIGO_RESUME.pdf";
+import data from "./about-data";
 
 const About = () => {
-  const [paragraphs, setParagraphs] = useState(data)
+  const [paragraphs, setParagraphs] = useState(data);
   return (
-    <section id='about'>
-      <div className='container about__container'>
-        <div className='section__title'>
+    <section id="about">
+      <div className="container about__container">
+        <div className="section__title">
           <h2>About Me</h2>
-          <div className='underline'></div>
+          <div className="underline"></div>
         </div>
-        <div className='about__top'>
-          <div className='about__left'>
+        <div className="about__top">
+          <div className="about__left">
             <h4>Get to Know Me!</h4>
-            <div className='about__left-paragraphs'>
-              <p>
+            <div className="about__left-paragraphs">
+              {/* <p>
                 Hello! My name is Bawo. I’m a dedicated Ux/Ui designer. I enjoy
                 designing clean, user-intuitive websites, web applications and
                 moblie applications that leads to the success of the overall
@@ -48,28 +48,56 @@ const About = () => {
                 I'm open to Job opportunities where I can contribute, learn and
                 grow. If you have a good opportunity that matches my skills and
                 experience, then don't hesitate to contact me.
+              </p> */}
+
+              <p>
+                Hello! My name is Bawo, I am a highly motivated and experienced
+                Product Designer with a passion for helping businesses translate
+                their goals into user-centric solutions that are both functional
+                and aesthetically pleasing. I am skilled in designing intuitive
+                interfaces that enhance user satisfaction, conducting
+                comprehensive user research, and creating stunning prototypes
+                that bring your ideas to life.
+              </p>
+              <p>
+                With over four years in product design, I've created digital
+                products that resonate with users on an emotional level,
+                produced intuitive designs that make complex tasks feel
+                effortless, and generated positive user feedback driving
+                engagement metrics high. You can see some of my work in Projects
+              </p>
+
+              <p>
+                When not working you can find me exploring new tech trends and
+                drawing inspiration from everyday life which fuels my creativity
+                and keeps my design thinking fresh and innovative. You can
+                follow me on Instagram where I post useful content related to
+                Product Design. I believe every product has a story to tell and
+                I’m committed to helping you tell yours in a way that resonates
+                deeply with your users. Let’s connect to know how I can be of
+                help.
               </p>
             </div>
-            <a href='#contact' className='btn'>
+            <a href="#contact" className="btn">
               Contact Me
             </a>
           </div>
-          <div className='about__right'>
-            <div className='about__portrait'>
-              <img src={AboutImg} alt='' />
+          <div className="about__right">
+            <div className="about__portrait">
+              <img src={AboutImg} alt="" />
             </div>
           </div>
         </div>
-        <div className='about__bottom'>
+        <div className="about__bottom">
           <h4>My skills</h4>
           <Skills paragraphs={paragraphs}></Skills>
-          <a href={Cv} download className='btn'>
+          <a href={Cv} download className="btn">
             Download CV
           </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
 
 const Contact = () => {
   return (
-    <section id='contact'>
-      <div className='container contact__container'>
-        <div className='section__title'>
+    <section id="contact">
+      <div className="container contact__container">
+        <div className="section__title">
           <h2>contact</h2>
-          <div className='underline'></div>
+          <div className="underline"></div>
         </div>
-        <p className='contact__first-p'>
-          If you like what you are looking at, feel free to contact me through
-          any of the following and I’d get to you as soon as possible.
+        <p className="contact__first-p">
+          For this “ Love what you see? Let’s make something amazing together!
+          Reach out through any of the following channels below, and I’ll be in
+          touch immediately 🚀
         </p>
-        <div className='contact__btns'>
+        <div className="contact__btns">
           <a
-            href='mailto:otaighobawo@gmail.com'
-            target='_blank'
-            className='btn'
+            href="mailto:otaighobawo@gmail.com"
+            target="_blank"
+            className="btn"
           >
             Email
           </a>
           <a
-            href='https://www.linkedin.com/in/bawo-otaigho-b49a69167/'
-            target='_blank'
-            className='btn white'
+            href="https://www.linkedin.com/in/bawo-otaigho-b49a69167/"
+            target="_blank"
+            className="btn white"
           >
             Linkedin
           </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
